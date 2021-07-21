1996a0fa1bc484402661e9e9a1894819399b1ea1
Ng Jing Cheng <jcng2308@gmail.com>
2021-07-22 01:28:39 +0800

    api schema

--- amplify/backend/function/vcryptoe5357f35PostConfirmation/src/custom.js
+++ amplify/backend/function/vcryptoe5357f35PostConfirmation/src/custom.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
 
   // Save the user to DynamoDB
   const date = new Date();
-  
+
   const Item = {
     'id': { S: event.request.userAttributes.sub },
     '__typename': { S: 'User' },
@@ -34,9 +34,28 @@ exports.handler = async (event, context) => {
     TableName: process.env.USERTABLE,
   }
 
-
   try {
     await ddb.putItem(params).promise();
+    console.log("Success");
+  } catch (e) {
+    console.log("Error", e);
+  }
+
+  const PortfolionCoinItem = {
+    'id': { S: `${event.request.userAttributes.sub}-usd` },
+    '__typename': { S: 'PortfolioCoin' },
+    'createdAt': { S: date.toISOString() },
+    'updatedAt': { S: date.toISOString() },
+    'userId': { S: event.request.userAttributes.sub },
+    'coinId': { S: process.env.USD_COIN_ID },
+    'amount': { N: "100000.0" }
+  }
+
+  try {
+    await ddb.putItem({
+      Item: PortfolionCoinItem,
+      TableName: process.env.PORTFOLIO_COIN_TABLE,
+    }).promise();
     console.log("Success");
   } catch (e) {
     console.log("Error", e);
